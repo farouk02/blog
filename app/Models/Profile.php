1e5ed9399d25b6b1bc23402528fa68dc1909e0e8
Farouk KIOUS <faroukious0@gmail.com>
2022-01-21 14:54:11 +0100

    prepare routes

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Profile extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'user_id',
@@ -21,4 +23,9 @@ class Profile extends Model
     protected $dates = [
         'birth_day',
     ];
+
+    // public function user(): BelongsTo
+    // {
+    //     return $this->belongsTo(User::class, 'user_id');
+    // }
 }
